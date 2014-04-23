ac460268e4efb804c757ddadd33182897b42b2b9
Stuart <stu@16pixels.com.au>
2014-04-23 15:01:33 +1000

    More Specific Leaving times
    Updated to specific times not just general. Makes a better API call
    that is more accurate in the information that is returned.

--- api/getTimes/index.php
+++ api/getTimes/index.php
@@ -7,9 +7,12 @@ $utcTime = new UtcTime();
 
 switch ($_GET['apiCall']) {
 	case 'getTimes':
+		//get general leaving times for the stop.
 		$jsonResult = $api->getTimes($_GET['startID']);
 		$timeArray = json_decode($jsonResult,true);
-		//echo "<pre>".print_r($timeArray)."</pre>";
+		//echo "<pre>";
+		//print_r($timeArray);
+		//echo "</pre>";
 		foreach ($timeArray['values'] as $train) {
 			// Now check if train is stopping at destination
 			$jsonResult = $api->getStoppingPattern($_GET['startID'],$train['run']['run_id']);
@@ -18,9 +21,26 @@ switch ($_GET['apiCall']) {
 			foreach ($stopArray['values'] as $stop) {
 				if ($stop['platform']['stop']['stop_id'] == $_GET['stopID']) {
 					if($utcTime->timeInFuture($stop['time_timetable_utc'])) {
-						$trainArray[] = array('departureTime' => $utcTime->convertUTC($train['time_timetable_utc']), 'arrivalTime' => $utcTime->convertUTC($stop['time_timetable_utc']));
-						//echo $utcTime->convertUTC($train['time_timetable_utc']).' - '.$utcTime->convertUTC($stop['time_timetable_utc']).'<br>';
+						$directionID = $stop['platform']['direction']['direction_id'];
+						$lineID = $stop['platform']['direction']['line']['line_id'];
 					}
+				}
+			}
+		}
+		$jsonResult = $api->getSpecificTimes($lineID,$_GET['startID'],$directionID);
+		$timeArray = json_decode($jsonResult,true);
+		//echo "<pre>";
+		//print_r($timeArray);
+		//echo "</pre>";
+		foreach($timeArray['values'] as $train) {
+			//Now need to find arrival time for each train
+			//echo 'stuff'.$train['run']['run_id'];
+			$jsonResult = $api->getStoppingPattern($_GET['startID'],$train['run']['run_id']);
+			$stopArray = json_decode($jsonResult,true);
+			//make sure train stops at destination
+			foreach($stopArray['values'] as $trainStation) {
+				if ($trainStation['platform']['stop']['stop_id'] == $_GET['stopID']) {
+					$trainArray[] = array('departureTime' => $utcTime->convertUTC($train['time_timetable_utc']), 'arrivalTime' => $utcTime->convertUTC($trainStation['time_timetable_utc']));
 				}
 			}
 		}
