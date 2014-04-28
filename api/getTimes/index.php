d54c4d711e0ba590960a7ecb82fce8f664fbaeb2
Stuart <stu@16pixels.com.au>
2014-04-28 20:50:12 +1000

    Fixed issues from Flinders St
    When getting train info for line Frankston from flinders st it will get
    all trains that go to caulfield even if the train does not go all the
    way to Frankston. To fix this needed to ask for more trains from the
    API.

--- api/getTimes/index.php
+++ api/getTimes/index.php
@@ -27,11 +27,9 @@ switch ($_GET['apiCall']) {
 				}
 			}
 		}
-		$jsonResult = $api->getSpecificTimes($lineID,$_GET['startID'],$directionID);
+		$jsonResult = $api->getSpecificTimes($lineID,$_GET['startID'],$directionID,5);
 		$timeArray = json_decode($jsonResult,true);
-		//echo "<pre>";
-		//print_r($timeArray);
-		//echo "</pre>";
+
 		foreach($timeArray['values'] as $train) {
 			//Now need to find arrival time for each train
 			//echo 'stuff'.$train['run']['run_id'];
@@ -40,7 +38,30 @@ switch ($_GET['apiCall']) {
 			//make sure train stops at destination
 			foreach($stopArray['values'] as $trainStation) {
 				if ($trainStation['platform']['stop']['stop_id'] == $_GET['stopID']) {
-					$trainArray[] = array('departureTime' => $utcTime->convertUTC($train['time_timetable_utc']), 'arrivalTime' => $utcTime->convertUTC($trainStation['time_timetable_utc']));
+					$trainArray[] = array('departureTime' => $utcTime->convertUTC($train['time_timetable_utc']), 'arrivalTime' => $utcTime->convertUTC($trainStation['time_timetable_utc']),'runID' => $train['run']['run_id']);
+					$timeCount++;
+				}
+			}
+		}
+		//If we have not found the required number of trains try getting more from API
+		if($timeCount < 5) {
+			$timeCount = 0;
+			$trainArray = array();
+			$jsonResult = $api->getSpecificTimes($lineID,$_GET['startID'],$directionID,10);
+			$timeArray = json_decode($jsonResult,true);
+
+			foreach($timeArray['values'] as $train) {
+				//Now need to find arrival time for each train
+				//echo 'stuff'.$train['run']['run_id'];
+				$jsonResult = $api->getStoppingPattern($_GET['startID'],$train['run']['run_id']);
+				$stopArray = json_decode($jsonResult,true);
+				foreach($stopArray['values'] as $trainStation) {
+					if ($trainStation['platform']['stop']['stop_id'] == $_GET['stopID']) {
+						if($timeCount < 5) {
+							$trainArray[] = array('departureTime' => $utcTime->convertUTC($train['time_timetable_utc']), 'arrivalTime' => $utcTime->convertUTC($trainStation['time_timetable_utc']),'runID' => $train['run']['run_id']);
+							$timeCount++;
+						}
+					}
 				}
 			}
 		}
